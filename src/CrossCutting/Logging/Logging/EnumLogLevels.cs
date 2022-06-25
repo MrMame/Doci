e2088875e444959d19033b7af9c4cfd393da0041
MrMame <51000524+MrMame@users.noreply.github.com>
2022-06-25 18:07:20 +0200

    Added CrossCutting.Logger Project

---
+++ src/CrossCutting/Logging/Logging/EnumLogLevels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkusMeinhard.Doci.CrossCutting.Logger
+{
+
+    public enum LogLevels{
+        Off,
+        Info,
+        Warning,
+        Error,
+        Fatal,
+        Debug,
+        All
+    }
+
+
+}
