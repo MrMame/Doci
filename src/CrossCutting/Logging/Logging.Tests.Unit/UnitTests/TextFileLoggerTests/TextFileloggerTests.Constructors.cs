665b1f3e81a8d531cb437e74e23b10057ed72989
MrMame <51000524+MrMame@users.noreply.github.com>
2022-06-26 22:01:24 +0200

    TextFilelogger.Tests class created

---
+++ src/CrossCutting/Logging/Logging.Tests.Unit/UnitTests/TextFileLoggerTests/TextFileloggerTests.Constructors.cs
@@ -0,0 +1,19 @@
+﻿
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarkusMeinhard.Doci.CrossCutting.Logging.Tests.Unit.UnitTests.TextFileLoggerTests
+{
+    public partial class TextFileLoggerTests
+    {
+
+        [Test]
+        public void ConstructorParameterless_DefaultLoglevel_IsALL ()
+        {
+            Assert.IsTrue (false);
+        }
+
+    }
+}
---
+++ src/CrossCutting/Logging/Logging.Tests.Unit/UnitTests/TextFileLoggerTests/TextFileloggerTests.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace MarkusMeinhard.Doci.CrossCutting.Logging.Tests.Unit.UnitTests.TextFileloggerTests
+{
+    public partial class TextFileLoggerTests
+    {
+        [SetUp]
+        public void Setup ()
+        {
+        }
+
+       [TearDown]
+       public void TearDown()
+        {
+        }
+    }
+}
