6e9fa6ea336987cf2890c2a4151ccca97f08447d
MrMame <51000524+MrMame@users.noreply.github.com>
2022-06-27 10:04:47 +0200

    Added Test LogText_RunningTest_WillAlwaysFail to check if github actions runner will show the test error.

---
+++ src/CrossCutting/Logging/Logging.Tests/UnitTests/TextFileLoggerTests/TextFileLoggerTests.LogText.cs
@@ -0,0 +1,28 @@
+﻿
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Mame.Doci.CrossCutting.Logging.Loggers;
+using System.IO;
+
+namespace Mame.Doci.CrossCutting.Logging.Tests.UnitTests.TextFileLoggerTests
+{
+    [TestFixture]
+    public partial class TextFileLoggerTests
+    {
+
+
+        [Test]
+        public void LogText_RunningTest_WillAlwaysFail ()
+        {
+            //Arrange
+            //Act
+            //Assert
+            Assert.IsTrue (false);
+        }
+
+
+
+    }
+}
