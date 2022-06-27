0e6afb420eec560389c3dfb46d5c96e0149d3958
MrMame <51000524+MrMame@users.noreply.github.com>
2022-06-27 11:41:52 +0200

    Added MultiLoggerTests with placeholder Tests inside. Renamed LogfilesController.cs Filename into MultiLogger.cs.

---
+++ src/CrossCutting/Logging/Logging.Tests/UnitTests/MultiLoggerTests/MultiLoggerTests.AddLogger.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mame.Doci.CrossCutting.Logging.Tests.UnitTests.MultiLoggerTests
+{
+    [TestFixture]
+    public partial class MultiLoggerTests
+    {
+        [Test]
+        public void AddLogger_IfNULLParameter_ThrowsException ()
+        {
+            //ARRANGE
+            //ACT
+            //ASSERT
+            Assert.Fail ("TestNotImplemented");
+        }
+
+
+    }
+}
---
+++ src/CrossCutting/Logging/Logging.Tests/UnitTests/MultiLoggerTests/MultiLoggerTests.LogText.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mame.Doci.CrossCutting.Logging.Tests.UnitTests.MultiLoggerTests
+{
+    [TestFixture]
+    public partial class MultiLoggerTests
+    {
+        [Test]
+        public void LogText_IfLoggersAdded_CallLogTextOnAllLoggersFromList ()
+        {
+            //ARRANGE
+            //ACT
+            //ASSERT
+            Assert.Fail ("TestNotImplemented");
+        }
+        [Test]
+        public void LogText_IfNoLoggersAdded_NothingHappens ()
+        {
+            //ARRANGE
+            //ACT
+            //ASSERT
+            Assert.Fail ("TestNotImplemented");
+        }
+
+    }
+}
