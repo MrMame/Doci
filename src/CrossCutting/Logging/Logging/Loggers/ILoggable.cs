dce5c152c147367870cf9c9e84606d3150f05dcd
MrMame <51000524+MrMame@users.noreply.github.com>
2022-06-28 14:12:17 +0200

    added ILoggable Interface

---
+++ src/CrossCutting/Logging/Logging/Loggers/ILoggable.cs
@@ -0,0 +1,15 @@
+﻿using Mame.Doci.CrossCutting.Logging.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mame.Doci.CrossCutting.Logging.Loggers
+{
+    public interface ILoggable
+    {
+        ILogger Logger { get;set;}      // Logger that will be used to Log the Message
+        void LogMessage (LogLevels LogLevel,string Message);    // Call if you want to log a Message
+
+    }
+}
