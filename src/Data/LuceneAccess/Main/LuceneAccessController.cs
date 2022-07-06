4361baa2c1528137f83f4801ba636ef37127b5cd
MrMame <51000524+MrMame@users.noreply.github.com>
2022-07-06 18:20:49 +0200

    Added Logic.DocumentAccessing Assembly to provide public interfaces for LuceneAccessController

---
+++ src/Data/LuceneAccess/Main/LuceneAccessController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mame.Doci.Data.LuceneAccess.Main
+{
+    class LuceneAccessController
+    {
+    }
+}
