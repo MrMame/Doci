8986c7969b27c7a19e12f589e3fe1e724fc2e0dd
MrMame <51000524+MrMame@users.noreply.github.com>
2022-07-13 22:02:17 +0200

    added logmessages and storedocuemntforuser function

--- src/UI/ConsoleClient/Program.cs
+++ src/UI/ConsoleClient/Program.cs
@@ -16,24 +16,52 @@ namespace Mame.Doci.UI.ConsoleClient
 
         static void Main (string[] args)
         {
-            // Create Logging
+            // Init Logging Objects
             ILogger logger = new ConsoleLogger ();
 
             // Parsing commandline
             var clp = new CLParsing.CommandLineArgumentsParser (args);
-
-            if (clp.HasParsingErrors == true){
-                logger.LogText(LogLevels.Error, "Invalid commandline parameters detected!");
-            }
-            else if (!string.IsNullOrEmpty (clp.AddDocument))
+            if (clp.HasParsingErrors == true)
             {
-                logger.LogText (LogLevels.Info, "...Start adding file (" + clp.AddDocument + ")");
-                var documentFilename = new FileInfo(clp.AddDocument);
-
-                IStoringForUser documentStoreController = StoringControllerFactory.CreateDefault (logger);
-                documentStoreController.UserWantsToStore (documentFilename);
+                logger.LogText(LogLevels.Error, "Invalid commandline parameters detected!");
+                return;
             }
+            // Start doing commands
+            // -> AddDcoument
+            if (!string.IsNullOrEmpty (clp.AddDocument))
+            {
+                StoreDocumentForUser (logger,clp.AddDocument);
+            }
+            // End
+            logger.LogText (LogLevels.Info, "Program finished!");
+        }
 
+
+
+
+
+        private static void StoreDocumentForUser (ILogger logger,string documentFileName)
+        {
+            // Parameter Check
+            if (string.IsNullOrEmpty (documentFileName)) throw new ArgumentNullException ();
+            // Get File to add
+            logger.LogText (LogLevels.Info, "...Start adding file (" + documentFileName + ")");
+            // Return if file is not existing
+            var documentFile = new FileInfo (documentFileName);
+            if (!documentFile.Exists)
+            {
+                logger.LogText (LogLevels.Info, "File to add is not existing (" + documentFile.FullName + ")!");
+                return;
+            }
+            // Start storing
+            try
+            {
+                IStoringForUser documentStoreController = StoringControllerFactory.CreateDefault (logger);
+                documentStoreController.UserWantsToStore (documentFile);
+            } catch (Exception ex)
+            {
+                logger.LogText (LogLevels.Fatal, "An unhandled exception occures while storing the document. " + ex.Message);
+            }
         }
 
     }
