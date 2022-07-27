0e1554726e6ef81e016b934915a196ee22b47ece
MrMame <51000524+MrMame@users.noreply.github.com>
2022-07-27 19:42:19 +0200

    Created Logging.Contracts.Exceptions.LogMessageException

---
+++ src/CrossCutting/Logging.Contracts/Exceptions/LogMessageException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mame.Doci.CrossCutting.Logging.Contracts.Exceptions
+{
+    [Serializable]
+    internal class LogMessageException:Exception
+    {
+        LogMessageException ()
+        {
+        }
+
+        LogMessageException (string message)
+            : base (message)    
+        {
+        }
+
+        LogMessageException (string message, Exception innerException)
+            : base (message, innerException)
+        {
+        }
+
+    }
+}
