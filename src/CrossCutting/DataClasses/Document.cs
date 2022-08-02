5c56118b676ab661e31ccb2c790c899c9765419e
MrMame <51000524+MrMame@users.noreply.github.com>
2022-08-02 10:36:01 +0200

    added CrossCutting.DataClases.Document

---
+++ src/CrossCutting/DataClasses/Document.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mame.Doci.CrossCutting.DataClasses
+{
+    public class Document
+    {
+
+        FileInfo _fileInfo;
+
+        public string Title { get; set; }
+        public string Path { get; set; }
+        public string FullFilename { get; set; }
+        public string FileExtension { get; set; }
+        public string FileSize { get; set; }
+        public string LastModified { get; set; }
+
+        public Document (FileInfo fileInfo)
+        {
+            if (fileInfo == null) throw new ArgumentNullException ();
+            _fileInfo = fileInfo;
+
+            this.Title = _fileInfo.Name;
+            this.FullFilename = _fileInfo.FullName;
+            this.FileExtension = _fileInfo.Extension;
+            this.Path = _fileInfo.DirectoryName;
+            this.FileSize = _fileInfo.Length.ToString ();
+            this.LastModified = _fileInfo.LastWriteTime.ToString ();
+
+        }
+    }
+}
